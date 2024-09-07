37131c74b39a8af34691a5bfb1716c94299009cc
Muhammad Awais <iawaisnaeem@gmail.com>
2024-09-08 00:46:04 +0500

    Secured Api Keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:news_app_flutter/view_view_model/auth_view_view_model.dart';
-import 'firebase_options.dart'; // Ensure this import matches the location of your firebase_options.dart file
+import 'firebase_options.dart'; // Make sure this is the correct path
 import 'package:news_app_flutter/utils/routes/routes.dart';
 import 'package:news_app_flutter/utils/routes/routes_name.dart';
 import 'package:news_app_flutter/view_view_model/category_view_view_model.dart';
 import 'package:news_app_flutter/view_view_model/news_view_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv for environment variables
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter bindings are initialized
+
+  // Load environment variables (ensure your .env file is properly set up)
+  await dotenv.load();
+
+  // Initialize Firebase with secure Firebase options from environment variables
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  ); // Initialize Firebase
+    options: DefaultFirebaseOptions.currentPlatform, // Use secure options
+  );
 
   runApp(const MyApp()); // Run the app after Firebase initialization
 }
@@ -24,15 +30,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthViewViewModel()),
-        ChangeNotifierProvider(create: (_) => NewsViewViewModel()),
-        ChangeNotifierProvider(create: (_) => CategoryViewViewModel())
+        ChangeNotifierProvider(create: (_) => AuthViewViewModel()), // AuthViewModel
+        ChangeNotifierProvider(create: (_) => NewsViewViewModel()), // NewsViewModel
+        ChangeNotifierProvider(create: (_) => CategoryViewViewModel()), // CategoryViewModel
       ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false, // Disable debug banner
         title: 'News App',
-        initialRoute: RouteName.SplashView,
-        onGenerateRoute: Routes.generateRoute,
+        initialRoute: RouteName.SplashView, // Set initial route to SplashView
+        onGenerateRoute: Routes.generateRoute, // Route generation logic
       ),
     );
   }
